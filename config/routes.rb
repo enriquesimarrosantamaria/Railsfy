1eed61546610e9b0d7e6811ccd7fe8c1b05cf6d4
Enrique Simarro Santamaría <enrique.simarro.santamaria@gmail.com>
2017-04-29 17:04:15 +0200

    Route changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get '/profile' => 'pages#profile'
 
-  get '/explore' => 'pages#explore'
+  get '/explore' => 'pages#explore' 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
